a26595bf5a404109aa0be10e24bdaa474bee8fe2
Akhil M Mani <akhilmmani2011@gmail.com>
2026-02-06 13:15:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/booking/[id]/success/page.tsx
+++ src/app/booking/[id]/success/page.tsx
@@ -2,4 +2,3 @@
 // The presence of a page at /booking/[id]/success was conflicting with
 // /booking/[eventId]/[bookingId]/success due to different dynamic slug names ('id' vs 'eventId').
 // By making this file not export a default component, we prevent Next.js from creating the conflicting route.
-export {};
