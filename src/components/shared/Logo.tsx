ec2816cecc0fbcdd0231778a7ffde1edecc7c258
Akhil M Mani <akhilmmani2011@gmail.com>
2026-02-07 07:30:29 +0000

    same issue Image with src "https://i.ibb.co/9vZbJgG/club7-logo.png" has

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -4,14 +4,16 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 group">
-      <Image
-        src="https://i.ibb.co/9vZbJgG/club7-logo.png"
-        alt="Club 7 Entertainments"
-        width={180}
-        height={45}
-        className="object-contain"
-        priority
-      />
+      <div>
+        <Image
+          src="https://i.ibb.co/9vZbJgG/club7-logo.png"
+          alt="Club 7 Entertainments"
+          width={180}
+          height={45}
+          className="object-contain"
+          priority
+        />
+      </div>
     </Link>
   );
 }
